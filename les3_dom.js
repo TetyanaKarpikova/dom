94b07a853c5b5ad1f2d03c1453ded4fdd7376184
Tetyana <tetyanajkn@gmail.com>
2020-11-02 19:05:17 +0200

    first commit

---
+++ les3_dom.js
@@ -0,0 +1,52 @@
+// Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// 1- отримує текст з параграфа з id "content"
+// let txt_content = document.getElementById('content')
+// console.log(txt_content.textContent);
+
+
+// 2- отримує текст з блоку з id "rules"
+// let txt_rules = document.getElementById('rules')
+// console.log(txt_rules.textContent);
+
+
+// 3- замініть текст параграфа з id 'content' на будь-який інший
+// let txt_content = document.getElementById('content')
+// txt_content.innerText = 'Просто текст.'
+
+
+// 4- замініть текст параграфа з id 'rules' на будь-який інший
+// let txt_rules = document.getElementById('rules')
+// txt_rules = document.innerText = 'Quia excepturi amet quo! Sapiente, quae?';
+
+
+// 5- змініть кожному елементу колір фону на червоний
+// let elem = document.body.getElementsByTagName('*');
+// for (const val of elem){
+//     val.style.backgroundColor = 'red'
+// }
+
+
+// 6- змініть кожному елементу колір тексту на синій
+// let elem_color = document.getElementsByTagName('*');
+// for (const value of elem_color){
+//     value.style.color = 'blue'
+// }
+
+
+// 7- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// let elem7 = document.getElementById('rules');
+// console.log(elem7.className);
+
+
+// 8- отримати всі елементи з класом fc_rules
+// let elem8 = document.getElementsByClassName('fc_rules');
+// for (const value of elem8){
+//     console.log(value)
+// }
+
+
+// 9- поміняти колір тексту у всіх елементів fc_rules на червоний
+// let elem9 = document.getElementsByClassName('fc_rules');
+// for (const value of elem9){
+//     value.style.color = 'red'
+// }
